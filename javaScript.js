c833d135d0d4d88107b3ec0a9e618fb1451a8c02
Ankit Shrestha <74950690+Ankit-Shrestha911@users.noreply.github.com>
2021-05-21 15:37:35 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,118 @@
+
+
+const icon = document.getElementById('icon-plus');
+const add = document.querySelector('#plus');
+
+// const show = () => {
+//     icon.style.display = "inline";
+// }
+
+// const hide = () => {
+//     icon.style.display = "none";
+// }
+
+const hideBtn = () => {
+    add.style.display = "none";
+}
+
+const showBtn = () => {
+    add.style.display = "flex";
+}
+
+const updateLocalStorage = () => {
+    const textAreaData = document.querySelectorAll('textarea');
+    const notes = [];
+    console.log(textAreaData);
+    textAreaData.forEach((note) => {
+        return notes.push(note.value);
+    })
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+    // textArea.disabled = true;
+}
+
+const addNewNote = (text = '') => {
+
+    const note = document.createElement('div');
+    note.classList.add('notes');
+
+    const htmlData = `
+              
+            <div class="upper-icon">
+            <i class="fas fa-edit"></i>
+            <i class="fas fa-save"></i>
+            <i class="fas fa-trash"></i>
+        </div>
+
+            <div class="text-content">
+            <div class="main" TextMode="MultiLine"">
+            </div>
+                <textarea placeholder="Write a note..."  onmouseover="hideBtn()" onmouseout="showBtn()"></textarea>
+            </div>
+    `;
+
+    note.insertAdjacentHTML('afterbegin', htmlData);
+
+    document.body.appendChild(note);
+
+    //References //
+    const edit = note.querySelector('.fa-edit');
+    const del = note.querySelector('.fa-trash');
+    const mainDiv = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+    const save = note.querySelector('.fa-save');
+
+    // Delete the note //
+
+    del.addEventListener('click', () => {
+        note.remove();
+        updateLocalStorage();
+    })
+
+
+    //toggle using edit button //
+
+    textArea.value = text;
+    textArea.innerHTML = text;
+
+    edit.addEventListener('click', () => {
+
+        textArea.disabled = false;
+
+
+
+        // textArea.classList.toggle('hidden');
+    })
+
+    save.addEventListener('click', () => {
+        textArea.disabled = true;
+        mainDiv.classList.toggle('hidden');
+
+    })
+
+
+    textArea.addEventListener('change', (event) => {
+        // const value = event.target.value;
+        textArea.innerHTML = event.target.value;;
+        if (textArea.value.length > 0) {
+            updateLocalStorage();
+        }
+    })
+
+    if (textArea.value.length != 0) {
+        textArea.disabled = true;
+    }
+
+}
+
+// getting a data from local storage //
+const notes = JSON.parse(localStorage.getItem('notes'));
+if (notes) {
+    notes.forEach((note) => {
+        addNewNote(note);
+    })
+}
+
+add.addEventListener('click', () => {
+    addNewNote()
+});
